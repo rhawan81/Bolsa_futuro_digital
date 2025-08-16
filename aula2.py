2acc27bea85b407fe8a0e3e29addbe4da7cfe33b
Jose Rhawan Michel Dos Santos Beltrão <rhawanjs@gmail.com>
2025-08-16 16:21:18 -0300

    Delete aula2.py

--- aula2.py
+++
@@ -1,3 +0,0 @@
-celsius = float(input('Informe a temperatura : ')) # entrada do usuario em celsius 
-fahreit = (celsius * 1.8) + 32 # -> sera convertida para fahreit 
-print(fahreit) # -> ira imprimir a entrada do usuario ja convertida  para celsius !

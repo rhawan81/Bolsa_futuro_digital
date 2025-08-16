33a35589b3961c577e5ddb6a4a87a57e8470e56d
Jose Rhawan Michel Dos Santos Beltrão <rhawanjs@gmail.com>
2025-08-16 16:21:43 -0300

    Delete aula5.py

--- aula5.py
+++
@@ -1,7 +0,0 @@
-nota1 = float(input('Informe uma nota :')) # 1 entrada do usuario para inserir notas
-nota2 = float(input('Informe uma nota :')) # 2 entrada do usuario para inserir notas
-nota3 = float(input('Informe uma nota :')) # 3 entrada do usuario para inserir notas
-
-media = (nota1 + nota2 + nota3) / 3
-resultado = media > 7
-print(media,resultado)

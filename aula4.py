207d541e0e7785219d1bad6cff96bdbdc872070f
Jose Rhawan Michel Dos Santos Beltrão <rhawanjs@gmail.com>
2025-08-16 16:21:36 -0300

    Delete aula4.py

--- aula4.py
+++
@@ -1,6 +0,0 @@
-number1 = int(input('Informe um Numero:'))
-number2 = int(input('Informe um Numero:'))
-soma = number1 + number2
-resultado = soma > 20
-print(f'A soma dos valores é de : {soma}')
-print(f'O numero é Maior que  20 ? {resultado}')
